f89587953ef1482c5f030b329663fe6379bd2f83
Ignacio Serrano <ign.serrano.g@gmail.com>
2026-02-17 01:24:49 -0300

    export firebaseapp for fcm

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -15,6 +15,8 @@ const firebaseConfig = {
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
+export const firebaseApp = app;
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app);
