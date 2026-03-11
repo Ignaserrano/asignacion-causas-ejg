fc33de8dc6c1be3e384c2e188504e1a0fe73527b
Ignacio Serrano <ign.serrano.g@gmail.com>
2026-03-10 21:36:19 -0300

    mejoras varias y modulo jurisprudencia

--- app/contacts/new/page.tsx
+++ app/contacts/new/page.tsx
@@ -61,7 +61,7 @@ export default function ContactNewPage() {
     return (
       <AppShell
         title="Nuevo contacto"
-        userEmail={null}
+       userEmail={user?.email ?? null}
         role={role}
         pendingInvites={pendingInvites}
         onLogout={doLogout}
